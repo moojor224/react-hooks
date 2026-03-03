2d48fff320bd19a47c5764106fcf593be6a66d32
moojor224 <moojor224@gmail.com>
2026-03-03 17:06:47 -0600

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,156 @@
+import { createContext, useContext, useMemo, useState } from "react";
+import { useDBCacheAdapter } from "./useIndexedDB";
+
+type IDBConfig = {
+    type: "IndexedDB";
+    database: string;
+    store: string;
+};
+type LocalStorageConfig = {
+    type: "LocalStorage";
+};
+type CustomConfig<T = any> = {
+    type: "custom";
+    adapter: (props: T) => StorageAdapter;
+    props: T;
+};
+type StorageConfig = IDBConfig | LocalStorageConfig | CustomConfig;
+
+export type StorageAdapter = {
+    /** size of the queue of pending storage operations */
+    workSize: number;
+    /** whether the storage adapter is ready to use */
+    ready: boolean;
+    /** true if the storage adapter had an error during initialization and can't be used */
+    error: boolean;
+    getValue<T>(key: string): T;
+    setValue(key: string, value: any): void;
+    deleteValue(key: string): void;
+    keys(): string[];
+};
+
+//! note: do not turn on react linting, or eslint will go crazy abount dynamically calling hooks
+/**
+ * Create a {@link StorageAdapter} from the given options.
+ * Options are locked in when useStorageAdapter is first called
+ */
+export function useStorageAdapter(config: StorageConfig): StorageAdapter {
+    const type = config.type;
+    const [adapterType] = useState(type);
+    if (adapterType !== type) {
+        console.warn("storage adapter type change from '%s' to '%s' detected. ignoring", adapterType, type);
+    }
+    switch (adapterType) {
+        case "IndexedDB": {
+            const idbConfig = config as IDBConfig;
+            // ensure that config values don't change if config changes
+            const [database] = useState(idbConfig?.database);
+            const [store] = useState(idbConfig?.store);
+            const dbAdapter = useDBCacheAdapter(database, store);
+            return dbAdapter;
+        }
+        case "LocalStorage": {
+            const adapter = useMemo<StorageAdapter>(
+                () =>
+                    ({
+                        ready: true,
+                        error: false,
+                        workSize: 0,
+                        deleteValue(key) {
+                            localStorage.removeItem(key);
+                        },
+                        getValue(key) {
+                            return JSON.parse(localStorage.getItem(key) ?? "undefined");
+                        },
+                        keys() {
+                            return new Array(localStorage.length).fill(0).map((_, i) => localStorage.key(i)!);
+                        },
+                        setValue(key, value) {
+                            localStorage.setItem(key, JSON.stringify(value));
+                        }
+                    }) satisfies StorageAdapter,
+                []
+            );
+            return adapter;
+        }
+        case "custom": {
+            const customConfig = config as CustomConfig<any>;
+            const [props] = useState(customConfig.props);
+            const adapter = customConfig.adapter(props);
+            return adapter;
+        }
+    }
+    // fallback
+    return localAdapter;
+}
+
+const localCache: Record<string, any> = {};
+const localAdapter: StorageAdapter = {
+    workSize: 0,
+    ready: true,
+    error: false,
+    getValue<T>(key: string): T {
+        return localCache[key];
+    },
+    setValue(key: string, value: any) {
+        localCache[key] = value;
+    },
+    deleteValue(key: string) {
+        delete localCache[key];
+    },
+    keys() {
+        return Object.keys(localCache);
+    }
+};
+
+/** Provides access to a {@link StorageAdapter} */
+export const StorageContext = createContext(localAdapter);
+
+function valueOrDefault<T>(val: T | null | undefined, def: T) {
+    if (val === null || val === undefined) {
+        return {
+            usedDefault: true,
+            value: def
+        };
+    }
+    return {
+        usedDefault: false,
+        value: val
+    };
+}
+
+/**
+ * Access a value stored in a storage adapter.
+ * Must be used inside of a {@link StorageContext} with a valid {@link StorageAdapter}
+ */
+export function useStoredValue<T>(key: string, initialValue: T) {
+    const storageAdapter = useContext(StorageContext);
+    const [stateVal, setStateVal] = useState(() => {
+        const val = valueOrDefault(storageAdapter.getValue<T>(key), initialValue);
+        if (val.usedDefault) {
+            storageAdapter.setValue(key, val.value);
+        }
+        return val.value;
+    });
+    /** keeps track of state */
+    const state = useMemo(
+        () => ({
+            value: stateVal
+        }),
+        []
+    );
+    return [
+        state.value,
+        function (arg0: T | ((last: T) => T)) {
+            let v: T;
+            if (typeof arg0 == "function") {
+                v = (arg0 as (last: T) => T)(state.value);
+            } else {
+                v = arg0;
+            }
+            storageAdapter.setValue(key, v);
+            setStateVal(v);
+            state.value = v;
+        }
+    ] as const;
+}
---
+++ src/useIndexedDB.ts
@@ -0,0 +1,154 @@
+import { ConcurrencyLimiter } from "@moojor224/promise-limiter";
+import { useEffect, useMemo, useState } from "react";
+import type { StorageAdapter } from ".";
+
+type DB =
+    | {
+          ready: true;
+          database: IDBDatabase;
+          error: boolean;
+      }
+    | {
+          ready: false;
+          database: null;
+          error: boolean;
+      };
+
+/** sets up connection to IDB */
+function useIndexedDB(dbName: string, storeName: string, version: number): DB {
+    const [db, setDB] = useState<IDBDatabase | null>(null);
+    const [error, setError] = useState(false);
+    useEffect(() => {
+        // effect runs once on mount
+        const request = indexedDB.open(dbName, version); // request open db
+        request.onupgradeneeded = function () {
+            // if successfully opened
+            request.result.createObjectStore(storeName); // create app's object store
+        };
+        request.onsuccess = function () {
+            setDB(request.result);
+        };
+        request.onerror = function () {
+            setError(true);
+        };
+    }, []);
+    return {
+        /** true if database is ready */
+        ready: !!db,
+        /** the database */
+        database: db,
+        /** whether the db init failed */
+        error
+    } as DB;
+}
+
+/** downloads database into cache object for instant getValue */
+function useDBCache(db: IDBDatabase | null, storeName: string) {
+    const [initialized, setInitialized] = useState(false);
+    const [error, setError] = useState<Error | null>(null);
+    const cache: Record<string, any> = useMemo(() => ({}), []);
+
+    useEffect(() => {
+        if (initialized || !db) return;
+        const tr = db.transaction(storeName, "readwrite");
+        const store = tr.objectStore(storeName);
+        // store.put("value", "key");
+        const keyreq = store.getAllKeys();
+        keyreq.onerror = function () {
+            setError(keyreq.error);
+        };
+        keyreq.onsuccess = function () {
+            const keys = keyreq.result.map((e) => e.toString());
+            Promise.allSettled<{ key: string; value: any }>(
+                keys.map(
+                    (e) =>
+                        new Promise((resolve, reject) => {
+                            const getreq = store.get(e);
+                            getreq.onsuccess = function () {
+                                resolve({ key: e, value: getreq.result });
+                            };
+                            getreq.onerror = function () {
+                                reject();
+                            };
+                        })
+                )
+            ).then((values) => {
+                values.forEach((v) => {
+                    if (v.status === "fulfilled") {
+                        cache[v.value.key] = v.value.value;
+                    }
+                });
+                setInitialized(true);
+            });
+        };
+    }, [db, initialized]);
+    return {
+        initialized,
+        cache,
+        error
+    };
+}
+
+const limit = new ConcurrencyLimiter(1);
+
+/** getters/setters for IDB cache */
+export function useDBCacheAdapter(dbName: string, storeName: string): StorageAdapter {
+    const database = useIndexedDB(dbName, storeName, 1);
+    const cache = useDBCache(database.database, storeName);
+    const [workSize, setWorkSize] = useState(0);
+    return {
+        workSize,
+        ready: database.ready && cache.initialized,
+        error: database.error || !!cache.error,
+        getValue<T>(key: string): T {
+            return cache.cache[key];
+        },
+        setValue(key: string, value: any) {
+            setWorkSize((e) => e + 1);
+            cache.cache[key] = value;
+            // queue database action
+            limit.run(
+                () =>
+                    new Promise<void>(function (resolve) {
+                        const tr = database.database!.transaction(storeName, "readwrite");
+                        const store = tr.objectStore(storeName);
+                        const setReq = store.put(value, key);
+                        setReq.onsuccess = function () {
+                            setWorkSize((e) => e - 1);
+                            resolve();
+                        };
+                        setReq.onerror = function () {
+                            console.error(setReq.error);
+                            setWorkSize((e) => e - 1);
+                            resolve();
+                        };
+                    })
+            );
+        },
+        deleteValue(key: string) {
+            setWorkSize((e) => e + 1);
+            delete cache.cache[key];
+            // queue database action
+            limit.run(
+                () =>
+                    new Promise<void>(function (resolve) {
+                        const tr = database.database!.transaction(storeName, "readwrite");
+                        const store = tr.objectStore(storeName);
+                        const delReq = store.delete(key);
+                        delReq.onsuccess = function () {
+                            setWorkSize((e) => e - 1);
+                            resolve();
+                        };
+                        delReq.onerror = function () {
+                            console.error(delReq.error);
+                            setWorkSize((e) => e - 1);
+                            resolve();
+                        };
+                    })
+            );
+        },
+        keys() {
+            return Object.keys(cache.cache);
+        }
+    };
+}
